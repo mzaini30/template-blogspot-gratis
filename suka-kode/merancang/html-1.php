8034a08139ef401076a5e4b9854ceeef89f65bcc
mzaini30 <muhzaini30@gmail.com>
2022-02-14 01:22:10 +0800

    Simpan

---
+++ suka-kode/merancang/html-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Document</title>
+
+	<!-- <link rel="stylesheet" href="https://assets.ubuntu.com/v1/vanilla-framework-version-3.1.0.min.css" /> -->
+	<link rel="stylesheet" href="vanilla-framework-version-3.1.0.min.css" />
+
+	<link rel="stylesheet" href="style.css" />
+
+</head>
+<body>
---
+++ suka-kode/merancang/index.php
@@ -0,0 +1,20 @@
+<?php include 'html-1.php' ?>
+
+<section class="p-strip--image is-light" style="background-image:url('cover.jpg')">
+  <div class="row">
+    <div class="">
+      <a href="/" class="p-card--overlay judul">
+        <h1>Main Kode</h1>
+        <p class="p-card__content">Blog ini didedikasikan untuk memberikan pengetahuan kepada masyarakat dunia tentang dunia kode yang indah.</p>
+      </a>
+    </div>
+  </div>
+</section>
+
+<div class="isi">
+	<div class="kiri">satu</div>
+	<div class="tengah">dua</div>
+	<div class="kanan">tiga</div>
+</div>
+
+<?php include 'html-2.php' ?>
